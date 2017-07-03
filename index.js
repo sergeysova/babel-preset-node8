f20b5f34070b94b424b6918739e797fd4c4fa161
Sergey Sova <i.am@lestad.net>
2017-07-03 14:48:28 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    require('babel-plugin-transform-es2015-modules-commonjs'),
+    require('babel-plugin-transform-class-properties'),
+    require('babel-plugin-transform-object-rest-spread'),
+    require('babel-plugin-transform-export-extensions'),
+    require('babel-plugin-transform-async-generator-function'),
+  ],
+  env: {
+    production: {
+      plugins: [
+        require('babel-plugin-transform-es2015-block-scoping'),
+        [require('babel-plugin-transform-es2015-for-of'), { loose: true }],
+        require('babel-plugin-transform-inline-consecutive-adds'),
+        require('babel-plugin-minify-dead-code-elimination'),
+      ],
+    },
+  },
+}
